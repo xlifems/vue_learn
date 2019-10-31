9981e4020bb74fe1eb4ef0a4e83ed679c85ad5de
xlifems <adrianmasapico@gmail.com>
2019-10-31 09:54:27 -0500

    nuevo carpeta

---
+++ veu-hello-app/src/views/About.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="about">
+    <div v-for="post of posts" v-bind:key="post.id">
+      <h4>{{ post.title}}</h4>
+      <p>{{ post.body }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+const URI = "https://jsonplaceholder.typicode.com/posts";
+export default {
+  data() {
+    return {
+      posts: []
+    };
+  },
+  created() {
+    this.getPosts();
+  },
+  methods: {
+    async getPosts() {
+      const response = await fetch(URI);
+      const data = await response.json();
+      console.log(data);
+      this.posts = data;
+    }
+  }
+};
+</script>
